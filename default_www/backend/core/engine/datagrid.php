48db9c4854219cbd9737d585cb6f52c68de303e7
lowiebenoot <lowiebenoot@gmail.com>
2011-03-15 09:11:58 +0100

    added an extra parameter to setMassActionCheckboxes, to say which checkboxes should be checked

--- default_www/backend/core/engine/datagrid.php
+++ default_www/backend/core/engine/datagrid.php
@@ -368,7 +368,7 @@ class BackendDataGrid extends SpoonDataGrid
 	 * @param	string $value						The value for the checkbox.
 	 * @param	array[optional] $excludedValues		The values that should be excluded.
 	 */
-	public function setMassActionCheckboxes($column, $value, array $excludedValues = null)
+	public function setMassActionCheckboxes($column, $value, array $excludedValues = null, array $checkedValues = null)
 	{
 		// build label and value
 		$label = '<span class="checkboxHolder"><input type="checkbox" name="toggleChecks" value="toggleChecks" /></span>';
@@ -398,6 +398,26 @@ class BackendDataGrid extends SpoonDataGrid
 
 			// assign the stack to the datagrid template
 			$this->tpl->assign('excludedCheckboxesData', $excludedData);
+		}
+
+		// checked IDs found
+		if(!empty($checkedValues))
+		{
+			// fetch the datagrid attributes
+			$attributes = $this->getAttributes();
+
+			// set if needed
+			if(!isset($attributes['id'])) $this->setAttributes(array('id' => 'table_'. time()));
+
+			// fetch the datagrid attributes
+			$attributes = $this->getAttributes();
+
+			// build array
+			$checkedData['id'] = $attributes['id'];
+			$checkedData['JSON'] = json_encode($checkedValues);
+
+			// assign the stack to the datagrid template
+			$this->tpl->assign('checkedCheckboxesData', $checkedData);
 		}
 	}
 
