b016cc2fca0726c3c8ea8ad9af53d78aa9e38d16
lowiebenoot <lowiebenoot@gmail.com>
2011-05-10 15:45:14 +0200

    functions weren't in alphabetical order

--- default_www/frontend/modules/share/engine/model.php
+++ default_www/frontend/modules/share/engine/model.php
@@ -12,21 +12,6 @@
 class FrontendShareModel
 {
 	/**
-	 * Share setting exists?
-	 *
-	 * @return	bool
-	 * @param	int $id		The id of the share setting.
-	 */
-	public static function settingExists($id)
-	{
-		return (bool) FrontendModel::getDB()->getVar('SELECT i.id
-															FROM share_settings AS i
-															WHERE i.id = ?',
-															(int) $id);
-	}
-
-
-	/**
 	 * Increase the amount of clicks of a share setting.
 	 *
 	 * @return	void
@@ -39,6 +24,21 @@ class FrontendShareModel
                                                 SET i.num_clicks = i.num_clicks+1
                                                 WHERE i.id = ?',
                                                 (int) $id);
+	}
+
+
+	/**
+	 * Share setting exists?
+	 *
+	 * @return	bool
+	 * @param	int $id		The id of the share setting.
+	 */
+	public static function settingExists($id)
+	{
+		return (bool) FrontendModel::getDB()->getVar('SELECT i.id
+															FROM share_settings AS i
+															WHERE i.id = ?',
+															(int) $id);
 	}
 }
 
